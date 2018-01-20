e7f06ae843812b455599d3baf92fbe6829e8af06
Forrest <forrest321@gmail.com>
2018-01-20 15:01:18 -0600

    main.go sends a tweet!

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/ChimeraCoder/anaconda"
+)
+
+func main() {
+
+	anaconda.SetConsumerKey("your-consumer-key")
+	anaconda.SetConsumerSecret("your-consumer-secret")
+	api := anaconda.NewTwitterApi("your-access-token",
+		"your-access-token-secret")
+
+	api.PostTweet("Hello World from Golang! ", nil)
+}
