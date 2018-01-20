5c788ea53e0c58b31fda89c5eb8cb1a8935c3c52
Forrest <forrest321@gmail.com>
2018-01-20 15:03:15 -0600

    adding a comment to main for commit weirdness

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 
+	//adding a comment for git weirdness.
 	anaconda.SetConsumerKey("your-consumer-key")
 	anaconda.SetConsumerSecret("your-consumer-secret")
 	api := anaconda.NewTwitterApi("your-access-token",
